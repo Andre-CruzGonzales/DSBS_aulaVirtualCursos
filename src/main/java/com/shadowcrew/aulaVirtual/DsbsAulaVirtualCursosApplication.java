cd76f178b3584acb1fef29d0f33ba52ea303539a
Andre Cruz Gonzales <andre.cruz@cobraperu.com.pe>
2023-04-27 12:42:06 -0500

    initial commmit

---
+++ src/main/java/com/shadowcrew/aulaVirtual/DsbsAulaVirtualCursosApplication.java
@@ -0,0 +1,13 @@
+package com.shadowcrew.aulaVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbsAulaVirtualCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbsAulaVirtualCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadowcrew/aulaVirtual/DsbsAulaVirtualCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowcrew.aulaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbsAulaVirtualCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
